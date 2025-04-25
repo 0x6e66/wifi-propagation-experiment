38209d650739c4799dd9f0064b8d98867169a940
Frondorf, Niklas <niklas@frondorf.eu>
2025-04-25 14:22:44 +0200

    feat: added plotting for correlating loss of throughput

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 def main():
     # runtime_throughput()
     distance_throughput()
+    # rssi_throughput()
 
 
 def distance_throughput():
@@ -20,8 +21,49 @@ def distance_throughput():
 
     plot_throughput(ax[0], data)
     plot_rssi(ax[1], data)
-    # plot_snr(ax[2], data)
-    # plot_noise(ax[3], data)
+
+    plt.show()
+
+
+def rssi_throughput():
+    data = {
+        "friis": pd.read_csv("friis.csv").iloc[1:],
+        "three_log":  pd.read_csv("three_log.csv").iloc[1:],
+        "two_ray": pd.read_csv("two_ray.csv").iloc[1:],
+    }
+    lines = []
+
+    fig, ax0 = plt.subplots()
+    for model, df in data.items():
+        distance = df["distance"]
+        flow_throughput = df["flow_throughput"]
+
+        for i in range(1, len(flow_throughput)-1):
+            if abs(flow_throughput[i] - flow_throughput[i+1]) > 3:
+                x = (2*i+1) / 2
+                if x not in lines:
+                    lines.append(x)
+
+        ax0.plot(distance, flow_throughput, label=model)
+
+    ax0.set_xlabel("Distance (m)")
+    ax0.set_ylabel("Throughput (Mbps)")
+    ax0.legend()
+    ax0.grid(True)
+
+    lines.sort()
+
+    ax1 = ax0.twinx()
+
+    for model, df in data.items():
+        distance = df["distance"]
+        rssi = df["rssi_average"]
+
+        ax1.plot(distance, rssi, label=model)
+
+    ax1.vlines(x=lines, ymin=[-79, -81.5, -79, -81.5], ymax=[-60,-60,-60,-60], ls=":", colors="red")
+    ax1.hlines(y=[-77.4, -80.6, -81.96], xmin=[60, 80, 97], xmax=[150, 220, 245], ls=":", colors="red")
+    ax1.set_ylabel("Signal Strength (dBm)")
 
     plt.show()
 
@@ -40,19 +82,6 @@ def plot_throughput(ax: plt.Axes, data: dict):
     ax.grid(True)
 
 
-def plot_snr(ax: plt.Axes, data: dict):
-    for model, df in data.items():
-        distance = df["distance"]
-        snr = df["snr_average"]
-
-        ax.plot(distance, 1-snr, label=model)
-
-    ax.set_xlabel("Distance (m)")
-    ax.set_title("SNR")
-    ax.legend()
-    ax.grid(True)
-
-
 def plot_rssi(ax: plt.Axes, data: dict):
     for model, df in data.items():
         distance = df["distance"]
@@ -63,20 +92,6 @@ def plot_rssi(ax: plt.Axes, data: dict):
     ax.set_xlabel("Distance (m)")
     ax.set_ylabel("Signal Strength (dBm)")
     ax.set_title("RSSI")
-    ax.legend()
-    ax.grid(True)
-
-
-def plot_noise(ax: plt.Axes, data: dict):
-    for model, df in data.items():
-        distance = df["distance"]
-        noise = df["noise_average"]
-
-        ax.plot(distance, noise, label=model)
-
-    ax.set_xlabel("Distance (m)")
-    ax.set_ylabel("Signal Strength (dBm)")
-    ax.set_title("Noise")
     ax.legend()
     ax.grid(True)
 
